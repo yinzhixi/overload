c1062d25af7ecd2c116fdec552e739a465d346e8
yinzhixi <1564085412@qq.com>
2019-06-19 22:20:40 +0800

    数据库文件更新

--- OverLoad/db/20180927.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE `t_preview` 
-ADD COLUMN `deleted` tinyint(4) NOT NULL DEFAULT '0',
-ADD COLUMN `year` INT NULL AFTER `deleted`,
-ADD COLUMN `month` INT NULL AFTER `year`,
-ADD COLUMN `day` INT NULL AFTER `month`;
-
-update t_preview set year = year(dateTime),month = month(dateTime),day=day(dateTime);
